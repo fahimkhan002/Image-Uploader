490bffd56a8e9580bf3b3fc7e6c4e3aa5b7e6b5d
Fahim Khan <fahimkhan002@yahoo.com>
2023-07-26 13:11:36 +0500

    Update ImageUploader.js

--- ImageUploader.js
+++ ImageUploader.js
@@ -3,6 +3,7 @@ import saveFile from '@salesforce/apex/ImageUploaderHandler.saveFile';
 import setImageUrl from '@salesforce/apex/ImageUploaderHandler.setImageUrl';
 import deleteFiles from '@salesforce/apex/ImageUploaderHandler.deleteFiles';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { RefreshEvent } from "lightning/refresh";
 
 export default class ImageUploader extends LightningElement {
   @api recordId;
@@ -20,6 +21,8 @@ export default class ImageUploader extends LightningElement {
   @track showDiv1 = true;
   @track showDiv2 = false;
   @track imageUrl;
+  @track isDelete = false;
+  @track isFile = false;
   @api showFooter = false;
 
   connectedCallback() {
@@ -31,11 +34,13 @@ export default class ImageUploader extends LightningElement {
     if (event.target.files.length > 0) {
       if (event.target.files[0].type != 'image/jpeg' && event.target.files[0].type != 'image/png') {
         this.fileName = 'Invalid file type!!';
+        this.isFile = true;
         this.isTrue = true;
       } else {
         this.isTrue = false;
         this.filesUploaded = event.target.files;
         this.fileName = event.target.files[0].name;
+        this.isFile = true;
       }
     }
   }
@@ -43,8 +48,11 @@ export default class ImageUploader extends LightningElement {
   handleSave() {
     if (this.filesUploaded.length > 0) {
       this.uploadHelper();
+      this.isFile = true
     } else {
+      
       this.fileName = 'Please select a file to upload!!';
+      this.isFile = false;
     }
   }
 
@@ -58,7 +66,7 @@ export default class ImageUploader extends LightningElement {
         .catch(error => {
           this.dispatchEvent(
             new ShowToastEvent({
-              title: 'Error!!',
+              title: 'Error!',
               message: error.message,
               variant: 'error',
             }),
@@ -69,41 +77,72 @@ export default class ImageUploader extends LightningElement {
           this.filesUploaded = [];
         });
     } else {
+      this.isFile = true
       this.fileName = 'Please select a file to upload!!';
+      this.isFile = false;
     }
   }
 
   uploadHelper() {
-    this.file = this.filesUploaded[0];
-    if (this.file.size > this.MAX_FILE_SIZE) {
-      this.dispatchEvent(
-        new ShowToastEvent({
-          title: 'Error',
-          message: 'File Size is too large',
-          variant: 'error',
-        }),
-      );
-      return;
+    let imageFile = null;
+
+    // Find the first image in the filesUploaded array
+    for (let i = 0; i < this.filesUploaded.length; i++) {
+        if (this.filesUploaded[i].type.startsWith('image/')) {
+            imageFile = this.filesUploaded[i];
+            break;
+        }
     }
+
+    if (!imageFile) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error!!',
+                message: 'No image file found in the uploaded files.',
+                variant: 'error',
+            }),
+        );
+        return;
+    }
+
+    // Process the image file
+    this.file = imageFile;
+
+    // The rest of the code remains the same as before
+    if (this.file.size > this.MAX_FILE_SIZE) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error!!!',
+                message: 'File Size is too large',
+                variant: 'error',
+            }),
+        );
+        return;
+    }
+
     this.isTrue = true;
+    this.isDelete = true;
+    this.isFile = false;
     this.showLoadingSpinner = true;
     this.fileReader = new FileReader();
     this.fileReader.onloadend = (() => {
-      this.fileContents = this.fileReader.result;
-      let base64 = 'base64,';
-      this.content = this.fileContents.indexOf(base64) + base64.length;
-      this.fileContents = this.fileContents.substring(this.content);
-      this.saveToFile();
+        this.fileContents = this.fileReader.result;
+        let base64 = 'base64,';
+        this.content = this.fileContents.indexOf(base64) + base64.length;
+        this.fileContents = this.fileContents.substring(this.content);
+        this.saveToFile();
     });
     this.fileReader.readAsDataURL(this.file);
-  }
+}
+
 
   saveToFile() {
     saveFile({ recordId: this.recordId, strFileName: this.file.name, base64Data: encodeURIComponent(this.fileContents) })
       .then(result => {
         this.imageUrl = this.data;
-        //this.fileName = this.fileName + ' - Uploaded Successfully';
+       // this.fileName = this.fileName + ' - Uploaded Successfully';
         this.isTrue = false;
+        this.isDelete = false;
         this.showLoadingSpinner = false;
         this.filesUploaded = [];
         this.dispatchEvent(
@@ -115,6 +154,7 @@ export default class ImageUploader extends LightningElement {
         );
         // Refresh the data after successful upload
         this.populateImageUrl();
+        this.dispatchEvent(new RefreshEvent());
       })
       .catch(error => {
         this.dispatchEvent(
@@ -126,6 +166,7 @@ export default class ImageUploader extends LightningElement {
         );
         this.fileName = '';
         this.isTrue = false;
+        this.isDelete = false;
         this.showLoadingSpinner = false;
         this.filesUploaded = [];
       });
@@ -139,15 +180,10 @@ export default class ImageUploader extends LightningElement {
         this.imageUrl = data;
         this.showDiv1 = false;
         this.showDiv2 = true;
+        this.isDelete = true;
       }
     }).catch(error => {
-      this.dispatchEvent(
-        new ShowToastEvent({
-          title: 'Error!!',
-          message: error.message,
-          variant: 'error',
-        }),
-      );
+
     });
   }
 
@@ -159,6 +195,7 @@ export default class ImageUploader extends LightningElement {
         this.showDiv1 = true; 
         this.showDiv2 = false;
         this.isTrue = true;
+        this.isDelete = false;
         this.dispatchEvent(
           new ShowToastEvent({
             title: 'Success!!',
@@ -166,17 +203,21 @@ export default class ImageUploader extends LightningElement {
             variant: 'success',
           }),
         );
+        this.dispatchEvent(new RefreshEvent());
       })
       .catch(error => {
         this.dispatchEvent(
           new ShowToastEvent({
-            title: 'Error!!',
+            title: 'Error!!!!!',
             message: error.message,
             variant: 'error',
           }),
         );
       });
+      this.dispatchEvent(new RefreshEvent());
   }
+
+
 
   // ... (any other methods or lifecycle hooks)
 }
